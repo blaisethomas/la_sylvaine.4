c916c686bb633f182ed1166fce9628f2c927fa3d
Prelang Builder <builder@prelang.com>
2014-12-04 04:30:07 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Pupil' to allow for create, update

--- app/admin/pupil.rb
+++ app/admin/pupil.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Pupil do
+  permit_params :school, :class_form, :telephone_number, :email, :emergency_contact_name, :emergency_contact_relationship, :emergency_contact_telephone, :previous_dancing_schools, :most_recent_examination_taken, :exam_result, :medical_conditions, :first_name, :last_name, :birthday
 
 
   # See permitted parameters documentation:
